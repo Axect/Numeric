60cbd4b6c03b5e60c016c9524971b90e256700c4
Axect <edeftg@gmail.com>
2017-09-20 03:13:12 +0900

    Fix Main

--- array/cmd/main.go
+++ array/cmd/main.go
@@ -13,13 +13,6 @@ func main() {
 	A := array.Create(1, 1, 100)
 	B := array.Create(-100, 1, -1)
 	array.Write([]Vector{A, B}, "test.csv")
-
-	C := array.Zeros(10, 5)
-	fmt.Println(C)
-
-	D := array.Zeros(10, 1)
-	fmt.Println(D[0])
-
 	E := array.Zeros(2, 2)
 	E[0][0] = 1
 	E[0][1] = 2
