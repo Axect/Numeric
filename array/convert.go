03fd4410822085e1f05b5e831d2ebf33e8bd49fb
Axect <edeftg@gmail.com>
2017-09-23 02:17:35 +0900

    Int64 to Int

--- array/convert.go
+++ array/convert.go
@@ -1,10 +1,10 @@
 package array
 
-// Float2Int converts []float64 to []int64
-func Float2Int(A []float64) []int64 {
-	B := make([]int64, len(A), len(A))
+// Float2Int converts []float64 to []int
+func Float2Int(A []float64) []int {
+	B := make([]int, len(A), len(A))
 	for i, elem := range A {
-		B[i] = int64(elem)
+		B[i] = int(elem)
 	}
 	return B
 }
